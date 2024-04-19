e395acaa170c1ceff0c96ebc36e89bd6e8622e48
nahoxie <67507971+nahoxie@users.noreply.github.com>
2024-04-19 18:33:47 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-#import dbapi
+import sqlalchemy
 
 """
 # Welcome to the New S&OP Routing Generator!
@@ -16,13 +16,18 @@ import streamlit as st
 def main():
     st.title("Data Retrieval from Databricks Example")
 
-    # Connect to Databricks cluster
-    conn = dbapi.connect(host=HOST, token=DB_KEY)
 
-    # Execute SQL query to retrieve data
-    cursor = conn.cursor()
-    cursor.execute('Select machine_downtime_key,downtime_duration_hrs,site_key from dap_enterprise.machine_downtime limit 10')
-    data = cursor.fetchall()
+host = "adb-3247355026438266.6.azuredatabricks.net"
+http_path = "sql/protocolv1/o/3247355026438266/0624-130121-wads668"
+token = db_key
+
+# Create the SQLAlchemy engine
+engine = sqlalchemy.create_engine(
+    f"databricks+odbc://token:{token}@{host}{http_path}"
+)
+
+query = "Select machine_downtime_key,downtime_duration_hrs,site_key from dap_enterprise.machine_downtime limit 10"
+    data = pd.read_sql(query)
 
     # Display the retrieved data in Streamlit
     st.write("Data from Databricks:", data)
