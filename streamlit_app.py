6f7a5aaf1460091bca801590f9edf1a77ef203de
nahoxie <67507971+nahoxie@users.noreply.github.com>
2024-04-19 21:07:14 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,8 +28,7 @@ st.sidebar.header("Selection Criteria")
 first_filter = st.sidebar.multiselect('Select DPP',["DPP1","DPP2"])
 second_filter = st.sidebar.multiselect('Select Source',["OSW-HM"])
 
-def download_csv(df):
-    csv =  {"data1.csv": df_route.to_csv(index=False), 
+csv =  {"data1.csv": df_route.to_csv(index=False), 
           "data2.csv": df_data_planned_dt.to_csv(index=False)}
  
 
